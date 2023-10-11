8a85bd2bb276e594662a0b312d60f2efcafa5c03
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-10-11 11:38:51 -0300

    Adicionei função para colunas não usadas

--- data_cleaner_mari.py
+++ data_cleaner_mari.py
@@ -7,3 +7,9 @@ def del_unic_val(df):
         if len(unic) == 1:
             df.drop(column, axis = 1, inplace = True)
     return df
+
+def useless_column(df, useless):
+    df.drop(useless, axis = 1, inplace = True)
+    return df
+
+# useless = ["NATUREZA_JURIDICA", "NO_LOGRADOURO", "NU_ENDERECO", "NO_COMPLEMENTO", "NO_BAIRRO", "CO_CEP", "NU_TELEFONE", "NO_EMAIL"]
