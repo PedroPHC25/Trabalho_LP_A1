4af07fdfc1cab37fd945269894cea465fd9abe78
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-10-12 10:19:56 -0300

    Criei funções group_and_sum e date_conversor

--- data_cleaner_pedro.py
+++ data_cleaner_pedro.py
@@ -13,3 +13,11 @@ def reset_index(df):
 def not_necessary_columns(df, columns):
     df.drop(columns, axis = 1, inplace = True)
     return df
+
+def group_and_sum(df, column_group, column_sum):
+    df = df.groupby(column_group)[column_sum].sum().reset_index()
+    return df
+
+def date_conversor(df, column, date_format):
+    df[column] = pd.to_datetime(df[column], format = date_format)
+    return df
