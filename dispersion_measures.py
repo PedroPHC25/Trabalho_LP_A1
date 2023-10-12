50c3ca96279207133642bdbebbc7cb9f8e4a56c2
PAULA EDUARDA DE LIMA <paulaeduardalima09@gmail.com>
2023-10-12 16:23:15 -0300

    Delete arquivo que tinhe criado, mas foi desnecess

--- dispersion_measures.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from df_concatenator import data
-
-# def mean_column(df, column_group, column_mean):
-#     df = df.groupby(column_group)[column_mean].mean().reset_index()
-#     return df
-
-
-# print(mean_column(data, "UF", "UTI_ADULTO_EXIST"))
