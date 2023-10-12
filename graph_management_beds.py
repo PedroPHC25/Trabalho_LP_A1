b1070187f05979e86a763e63b493ad2b87cae7fe
PAULA EDUARDA DE LIMA <paulaeduardalima09@gmail.com>
2023-10-12 16:27:29 -0300

    Deletei duas funções que seriam desnecessárias

--- graph_management_beds.py
+++ graph_management_beds.py
@@ -1,33 +1,11 @@
+"""Módulo do gráfico "Leitos x Tipo de gestão"
+
+Este módulo contém as funções necessárias para a análise
+e para a geração da visualização dos dados sobre o total de 
+leitos nos hospitais do Brasil e os tipos de gestão dos hospitais.
+"""
+
 import pandas as pd
 # import matplotlib.pyplot as plt
 from df_concatenator import data
 
-def select_columns(df, column1, column2, *args):
-    """
-    Recebe um dataframe e seleciona as colunas desejadas, retorna outro df com essas colunas.
-
-    Parameters
-    ----------
-    df : DataFrame
-        Tipo do pandas.
-
-    column1 : coluna do DataFrame fornecido.
-
-    column2 : coluna do DataFrame fornecido 
-
-    *args : parametros opcional, para o caso
-    em que o número de colunas desejadas seja 
-    maior que 2. 
-    
-    Returns
-    -------
-    df : DataFrame contendo as coluas selecionadas.
-    
-    """
-    new_df = df[[column1, column2, *args]]
-    return new_df
-
-
-# dataframe = select_columns(data, "MUNICIPIO", "REGIAO", "TP_GESTAO", "LEITOS_SUS")
-
-# print(dataframe)
