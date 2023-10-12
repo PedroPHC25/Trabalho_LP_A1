b617b893e48703002e54ea5348f6060f15c32a16
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-10-12 15:12:11 -0300

    Comentei meu data_cleaner

--- data_cleaner_pedro.py
+++ data_cleaner_pedro.py
@@ -1,13 +1,14 @@
-import pandas as pd
-
+# Função para filtrar apenas as colunas necessárias para a análise e visualização
 def select_columns(df, columns):
     selected_columns = df[columns]
     return selected_columns
 
+# Função para resetar o index do dataframe
 def reset_index(df):
     df.reset_index(inplace = True)
     return df
 
+# Função para excluir colunas desnecessárias do dataframe
 def not_necessary_columns(df, columns):
     df.drop(columns, axis = 1, inplace = True)
     return df
