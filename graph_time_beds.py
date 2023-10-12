752b32fbd2c40e5e98f4dbf45b900367695f9c41
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-10-12 12:14:24 -0300

    Rascunhos de análise e de gráfico

--- graph_time_beds.py
+++ graph_time_beds.py
@@ -1,20 +1,62 @@
-# subdata = select_columns(data, ["REGIAO", "LEITOS_EXISTENTES"])
-# subdata = reset_index(subdata)
-# subdata = not_necessary_columns(subdata, "CNES")
-# subdata = group_and_sum(subdata, ["COMP", "REGIAO"], "LEITOS_EXISTENTES")
-# subdata = date_conversor(subdata, "COMP", "%Y%m")
+from data_cleaner_pedro import select_columns, reset_index, not_necessary_columns
+from df_concatenator import data
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# subdata.reset_index(inplace = True)
-# subdata.drop("CNES", axis = 1, inplace = True)
-# subdata = subdata.groupby(["COMP"])["LEITOS_EXISTENTES"].sum().reset_index()
-# subdata["COMP"] = pd.to_datetime(subdata["COMP"], format = "%Y%m")
+def group_and_sum(df, column_group, column_sum):
+    df = df.groupby(column_group)[column_sum].sum().reset_index()
+    return df
 
-# data_centrooeste = subdata[subdata["REGIAO"] == "CENTRO-OESTE"]
-# data_nordeste = subdata[subdata["REGIAO"] == "NORDESTE"]
-# data_norte = subdata[subdata["REGIAO"] == "NORTE"]
-# data_sudeste = subdata[subdata["REGIAO"] == "SUDESTE"]
-# data_sul = subdata[subdata["REGIAO"] == "SUL"]
+def date_conversor(df, column, date_format):
+    df[column] = pd.to_datetime(df[column], format = date_format)
+    return df
 
-# plt.figure(figsize = (15, 5))
+
+
+subdata = select_columns(data, ["LEITOS_EXISTENTES"])
+subdata = reset_index(subdata)
+subdata = not_necessary_columns(subdata, "CNES")
+subdata = group_and_sum(subdata, ["COMP"], "LEITOS_EXISTENTES")
+subdata = date_conversor(subdata, "COMP", "%Y%m")
+
+# print(subdata)
+
+# print(subdata[subdata["COMP"].dt.year == 2019]["LEITOS_EXISTENTES"].mean())
+# print(subdata[subdata["COMP"].dt.year == 2020]["LEITOS_EXISTENTES"].mean())
+# print(subdata[subdata["COMP"].dt.year == 2021]["LEITOS_EXISTENTES"].mean())
+# print(subdata[subdata["COMP"].dt.year == 2022]["LEITOS_EXISTENTES"].mean())
+# print(subdata[subdata["COMP"].dt.year == 2023]["LEITOS_EXISTENTES"].mean())
+
+# print(subdata[subdata["COMP"].dt.year == 2019]["LEITOS_EXISTENTES"].median())
+# print(subdata[subdata["COMP"].dt.year == 2020]["LEITOS_EXISTENTES"].median())
+# print(subdata[subdata["COMP"].dt.year == 2021]["LEITOS_EXISTENTES"].median())
+# print(subdata[subdata["COMP"].dt.year == 2022]["LEITOS_EXISTENTES"].median())
+# print(subdata[subdata["COMP"].dt.year == 2023]["LEITOS_EXISTENTES"].median())
+
+# print(subdata[subdata["COMP"].dt.year == 2019]["LEITOS_EXISTENTES"].std())
+# print(subdata[subdata["COMP"].dt.year == 2020]["LEITOS_EXISTENTES"].std())
+# print(subdata[subdata["COMP"].dt.year == 2021]["LEITOS_EXISTENTES"].std())
+# print(subdata[subdata["COMP"].dt.year == 2022]["LEITOS_EXISTENTES"].std())
+# print(subdata[subdata["COMP"].dt.year == 2023]["LEITOS_EXISTENTES"].std())
+
+# print(subdata[subdata["COMP"].dt.year == 2019]["LEITOS_EXISTENTES"].max())
+# print(subdata[subdata["COMP"].dt.year == 2020]["LEITOS_EXISTENTES"].max())
+# print(subdata[subdata["COMP"].dt.year == 2021]["LEITOS_EXISTENTES"].max())
+# print(subdata[subdata["COMP"].dt.year == 2022]["LEITOS_EXISTENTES"].max())
+# print(subdata[subdata["COMP"].dt.year == 2023]["LEITOS_EXISTENTES"].max())
+
+# print(subdata[subdata["COMP"].dt.year == 2019]["LEITOS_EXISTENTES"].min())
+# print(subdata[subdata["COMP"].dt.year == 2020]["LEITOS_EXISTENTES"].min())
+# print(subdata[subdata["COMP"].dt.year == 2021]["LEITOS_EXISTENTES"].min())
+# print(subdata[subdata["COMP"].dt.year == 2022]["LEITOS_EXISTENTES"].min())
+# print(subdata[subdata["COMP"].dt.year == 2023]["LEITOS_EXISTENTES"].min())
+
+# print(subdata)
+
+# print(data_2019)
+
+
 # plt.plot(subdata["COMP"], subdata["LEITOS_EXISTENTES"])
+
+
 # plt.show()
