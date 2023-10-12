527f769d55b844e0abf8d8240d0e9be5417394bb
Eduarda Mesquita <eduardammag@gmail.com>
2023-10-12 19:54:08 -0300

    alterações graf barras duda

--- graph_bar_eduarda.py
+++ graph_bar_eduarda.py
@@ -4,11 +4,38 @@ Este módulo contém a  função que gera o gráfico de barras que permite uma
 análise comparativa da quantidade total de leitos pediátricos por região 
 do Brasil.
 """
+def graph_bar(df, x_column, y_column, title, x_label, y_label, image_graph_name):
+    """
+    Gera um gráfico de barras a partir de um DataFrame.
 
-import pandas as pd
-import matplotlib.pyplot as plt
-from df_concatenator import data
-from matplotlib import ticker
+    :param df: DataFrame contendo os dados.
+    :type df: pandas.DataFrame
+    :param x_column: Nome da coluna para o eixo x.
+    :type x_column: str
+    :param y_column: Nome da coluna para o eixo y.
+    :type y_column: str
+    :param title: Título do gráfico.
+    :type title: str
+    :param x_label: Rótulo do eixo x.
+    :type x_label: str
+    :param y_label: Rótulo do eixo y.
+    :type y_label: str
+    :param image_graph_name: Nome do arquivo de imagem para salvar.
+    :type image_graph_name: str
+
+    :return: None
+    :rtype: None
+
+    Gera um gráfico de barras a partir dos dados no DataFrame fornecido. Salva a imagem
+    na pasta "graphs" com o nome especificado.
+
+    Exemplo de uso:
+    graph_bar(data, 'REGIAO', 'UTI_PEDIATRICO_EXIST',
+              'TOTAL DE LEITOS PEDIÁTRICOS DE UTI POR REGIÃO',
+              'Região', 'Total de leitos pediátricos',
+              'uti_pediatrico_por_regiao.jpg')
+    """
+    
 
 # Função para plotar um gráfico de barras
 def graph_bar(df, x_column, y_column, title, x_label, y_label, image_graph_name):
