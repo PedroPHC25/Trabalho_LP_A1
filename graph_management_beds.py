cc24f2324454ba955a2893ec8d186e099d59f1d0
PAULA EDUARDA DE LIMA <paulaeduardalima09@gmail.com>
2023-10-12 16:58:03 -0300

    Documente funcção delete_columns

--- graph_management_beds.py
+++ graph_management_beds.py
@@ -6,6 +6,23 @@ leitos nos hospitais do Brasil e os tipos de gestão dos hospitais.
 """
 
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from df_concatenator import data
 
+# Função para plotar um gráfico de barras
+def graph_line(df, x_column, y_column, title, x_label, y_label, image_graph_name):
+    # Ajustando o tamanho do gráfico
+    plt.figure(figsize = (10, 6))
+    plt.plot(df[x_column], df[y_column], linewidth = 3, color = "black")
+    # Configurando os textos
+    plt.title(title, fontsize = 16)
+    plt.xlabel(x_label, fontsize = 14)
+    plt.ylabel(y_label, fontsize = 14)
+    plt.tick_params(axis = "x", labelsize = 10)
+    plt.tick_params(axis = "y", labelsize = 10)
+    # Adicionando ponto como separador de milhar no eixo
+    formatter = ticker.FuncFormatter(lambda x, pos: "{:,.0f}".format(x).replace(",", "."))
+    plt.gca().yaxis.set_major_formatter(formatter)
+
+    plt.savefig(f"graphs/{image_graph_name}]")
+    plt.show()
