f4fa4886b21cd5c74bc90d7ba52c560dc4e2515c
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-10-13 11:13:02 -0300

    Adicionei tratamento de exceções nas funções

--- graph_bar_SUS.py
+++ graph_bar_SUS.py
@@ -12,25 +12,54 @@ import numpy as np
 import pandas as pd
 from typing import Optional
 
-# TODO adicionar tratamento de exceção, adicionar testes, adicionar docstring, adicionar a análise estatística
+# TODO adicionar tratamento de exceção, adicionar testes, adicionar docstring
 
 # Função recebe o df e retorna a mediana do tipo hospital requisitado
 def median_SUS(df: pd.DataFrame, tipo_hospital: str) -> float:
-    return df[tipo_hospital].median()
+    try:
+        return df[tipo_hospital].median()
+    except TypeError:
+        print("Argumentos inadequados")
+    except KeyError:
+        print("Chave fora da formatação necessária")
+    except:
+        print("Erro desconhecido")
+
 
 # Função recebe o df e retorna a média do tipo hospital requisitado
 def mean_SUS(df: pd.DataFrame, tipo_hospital: str) -> float:
-    return df[tipo_hospital].mean()
+    try:
+        return df[tipo_hospital].mean()
+    except TypeError:
+        print("Argumentos inadequados")
+    except KeyError:
+        print("Chave fora da formatação necessária")
+    except:
+        print("Erro desconhecido")
 
 # Função recebe o df e retorna o desvio padrão do tipo hospital requisitado
 def std_SUS(df: pd.DataFrame, tipo_hospital: str) -> float:
-    return df[tipo_hospital].std()
+    try:
+        return df[tipo_hospital].std()
+    except TypeError:
+        print("Argumentos inadequados")
+    except KeyError:
+        print("Chave fora da formatação necessária")
+    except:
+        print("Erro desconhecido")
 
 # Função recebe o df e retorna o máximo e mínimo do tipo hospital requisitado
 def max_min_SUS(df: pd.DataFrame, tipo_hospital: str) -> tuple:
-    min = df[tipo_hospital].min()
-    max = df[tipo_hospital].max()
-    return min, max
+    try:
+        min = df[tipo_hospital].min()
+        max = df[tipo_hospital].max()
+        return min, max
+    except TypeError:
+        print("Argumentos inadequados")
+    except KeyError:
+        print("Chave fora da formatação necessária")
+    except:
+        print("Erro desconhecido")
 
 # Função recebe o df e as colunas que serão as barras agrupadas 
 def graph_SUS(df: pd.DataFrame, 
@@ -39,30 +68,35 @@ def graph_SUS(df: pd.DataFrame,
               image_graph_name: Optional[str] = "graph_SUS", 
               width: Optional[str] = 0.2, 
               colors: Optional[list] = ['royalblue', 'lightseagreen', 'mediumpurple']) -> None:
-    # Definido o eixo x como os registros da tabela
-    x = np.arange(len(df))
+    try:
+        # Definido o eixo x como os registros da tabela   
+        x = np.arange(len(df))
 
-    # Atribuindo as colunas as suas respectivas barras
-    bar1 = df[y1]
-    bar2 = df[y2]
-    bar3 = df[y3]
+        # Atribuindo as colunas as suas respectivas barras
+        bar1 = df[y1]
+        bar2 = df[y2]
+        bar3 = df[y3]
 
-    # Título, legenda e ticks 
-    legend = [y1, y2, y3]
-    plt.legend(legend) 
-    plt.figure(figsize = (10, 6))
-    plt.title(title, fontsize = 16)
-    plt.xticks(x, df["Estado"]) 
-    
-    # Plotando as barras
-    plt.bar(x - width, bar1, width, color=colors[0])
-    plt.bar(x, bar2, width, color=colors[1])
-    plt.bar(x + width, bar3, width, color=colors[2])
-    
-    plt.savefig(f"graphs/{image_graph_name}")
-    plt.show()
-
-print(type(median_SUS(df_formatado, "Hospital Público")))
-print(type(mean_SUS(df_formatado, "Hospital Público")))
-print(type(std_SUS(df_formatado, "Hospital Público")))
-print(type(max_min_SUS(df_formatado, "Hospital Público")))
+        # Título, tamanho e ticks 
+        plt.figure(figsize = (10, 6))
+        plt.title(title, fontsize = 16)
+        plt.xticks(x, df["Estado"]) 
+            
+        # Plotando as barras
+        plt.bar(x - width, bar1, width, color=colors[0])
+        plt.bar(x, bar2, width, color=colors[1])
+        plt.bar(x + width, bar3, width, color=colors[2])
+            
+        # Adcicionando a legenda    
+        legend = [y1, y2, y3]
+        plt.legend(legend) 
+            
+        plt.savefig(f"graphs/{image_graph_name}")
+        plt.show()
+        
+    except KeyError:
+        print("Chave fora da formatação necessária")
+    except TypeError:
+        print("DataFrame inserido não válido")
+    except:
+        print("Erro desconhecido")
