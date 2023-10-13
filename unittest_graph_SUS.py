0d7644fe11cb871dcef2467e52e364af9b3b4101
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-10-13 13:31:24 -0300

    Adicionando assertivas

--- unittest_graph_SUS.py
+++ unittest_graph_SUS.py
@@ -6,6 +6,8 @@ class TestMedian_SUS(unittest.TestCase):
 
     def test_median_a(self):
         example = pd.DataFrame(columns=["a", "b", "c"], data=[[1, 2, 3], [5, 6, 8], [14, 12, 23]])
+        self.assertIsInstance(example, pd.DataFrame)
+        self.assertIsInstance(example["a"], pd.Series)
         result = median_SUS(example, "a")
         self.assertEqual(result, 5.0)
 
@@ -17,6 +19,7 @@ class TestMedian_SUS(unittest.TestCase):
     def test_invalid_key(self):
         example = pd.DataFrame(columns=["a", "b", "c"], data=[[1, 2, 3], [5, 6, 8], [14, 12, 23]])
         result = median_SUS(example, "d")
+        self.assertNotIn("d", example.columns)
         self.assertEqual(result, "Chave fora da formatação necessária")
 
     def test_invalid_arguments(self):
