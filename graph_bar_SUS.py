7de79b16a5ecb87cb237f066f81e231b65ac2fbd
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-10-13 10:34:58 -0300

    Adicionei comentários nas funções e melhorei hints

--- graph_bar_SUS.py
+++ graph_bar_SUS.py
@@ -10,41 +10,59 @@ from data_graph_SUS import df_formatado
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from typing import Optional
 
-def graph_SUS(df: pd.DataFrame, 
-              y1: str, y2: str, y3: str, 
-              title: str, image_graph_name: str, 
-              legend: list = ["Hospital Filantrópico", "Hospital Privado", "Hospital Público"], 
-              width: float = 0.2, 
-              colors: list = ['royalblue', 'lightseagreen', 'mediumpurple']) -> None:
-    x = np.arange(len(df))
-    plt.figure(figsize = (10, 6))
-    plt.title(title, fontsize = 16)
-    plt.bar(x - width, y1, width, color=colors[0])
-    plt.bar(x, y2, width, color=colors[1])
-    plt.bar(x + width, y3, width, color=colors[2])
-    plt.xticks(x, df["Estado"]) 
-    plt.legend(legend) 
-    plt.savefig(f"graphs/{image_graph_name}")
-    plt.show()
+# TODO adicionar tratamento de exceção, adicionar testes, adicionar docstring, adicionar a análise estatística
 
-# TODO adicionar comentários, adicionar tratamento de exceção, adicionar testes, adicionar docstring, adicionar a análise estatística
-
-def median_SUS(df: pd.DataFrame, tipo_hospital: str):
+# Função recebe o df e retorna a mediana do tipo hospital requisitado
+def median_SUS(df: pd.DataFrame, tipo_hospital: str) -> float:
     return df[tipo_hospital].median()
 
-def mean_SUS(df: pd.DataFrame, tipo_hospital: str):
+# Função recebe o df e retorna a média do tipo hospital requisitado
+def mean_SUS(df: pd.DataFrame, tipo_hospital: str) -> float:
     return df[tipo_hospital].mean()
 
-def std_SUS(df: pd.DataFrame, tipo_hospital: str):
+# Função recebe o df e retorna o desvio padrão do tipo hospital requisitado
+def std_SUS(df: pd.DataFrame, tipo_hospital: str) -> float:
     return df[tipo_hospital].std()
 
-def max_min_SUS(df: pd.DataFrame, tipo_hospital: str):
+# Função recebe o df e retorna o máximo e mínimo do tipo hospital requisitado
+def max_min_SUS(df: pd.DataFrame, tipo_hospital: str) -> tuple:
     min = df[tipo_hospital].min()
     max = df[tipo_hospital].max()
     return min, max
 
-print(median_SUS(df_formatado, "Hospital Público"))
-print(mean_SUS(df_formatado, "Hospital Público"))
-print(std_SUS(df_formatado, "Hospital Público"))
-print(max_min_SUS(df_formatado, "Hospital Público"))
+# Função recebe o df e as colunas que serão as barras agrupadas 
+def graph_SUS(df: pd.DataFrame, 
+              y1: str, y2: str, y3: str, 
+              title: Optional[str] = "Leitos SUS em janeiro 2022", 
+              image_graph_name: Optional[str] = "graph_SUS", 
+              width: Optional[str] = 0.2, 
+              colors: Optional[list] = ['royalblue', 'lightseagreen', 'mediumpurple']) -> None:
+    # Definido o eixo x como os registros da tabela
+    x = np.arange(len(df))
+
+    # Atribuindo as colunas as suas respectivas barras
+    bar1 = df[y1]
+    bar2 = df[y2]
+    bar3 = df[y3]
+
+    # Título, legenda e ticks 
+    legend = [y1, y2, y3]
+    plt.legend(legend) 
+    plt.figure(figsize = (10, 6))
+    plt.title(title, fontsize = 16)
+    plt.xticks(x, df["Estado"]) 
+    
+    # Plotando as barras
+    plt.bar(x - width, bar1, width, color=colors[0])
+    plt.bar(x, bar2, width, color=colors[1])
+    plt.bar(x + width, bar3, width, color=colors[2])
+    
+    plt.savefig(f"graphs/{image_graph_name}")
+    plt.show()
+
+print(type(median_SUS(df_formatado, "Hospital Público")))
+print(type(mean_SUS(df_formatado, "Hospital Público")))
+print(type(std_SUS(df_formatado, "Hospital Público")))
+print(type(max_min_SUS(df_formatado, "Hospital Público")))
