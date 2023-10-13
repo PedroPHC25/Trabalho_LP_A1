246458fe34069acfb84dd81fea93654f2696386a
PAULA EDUARDA DE LIMA <paulaeduardalima09@gmail.com>
2023-10-13 17:20:08 -0300

    Transferi a função para df_generator_functions

--- df_generator_functions.py
+++ df_generator_functions.py
@@ -133,3 +133,46 @@ def not_necessary_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
 
 if __name__ == "__main__":
     doctest.testmod(verbose = True)
+
+
+
+#____________________________PAULA______________________________________________________
+
+#Elimina coluna em que todas as células são NA
+def delete_columns_na(df: pd.DataFrame)-> pd.DataFrame:
+    """
+    Recebe um dataframe e elimina as colunas em que todos
+    os valores são NA.
+
+    :param df: DataFrame fornecido.
+
+    :type df: DataFrame  
+    
+    :return: Retorna outro DataFrame.
+    
+    A função recebrá um DataFrame e as colunas que tiverem 
+    TODOS os valores com NA serão removidas.
+
+    ..warning::
+    A mudança ocorre 'inplace', ou seja, modifica o dataframe 
+    original.
+
+    """
+    try:
+
+        df.dropna(how = "all", axis = 1, inplace = True)
+        return df
+    except AttributeError:
+        return "Argumento 'df' não é um dataframe"
+    except:
+        return "Erro desconhecido"
+    
+#__________________________________________________________________________________________________
+
+
+
+
+
+
+
+
