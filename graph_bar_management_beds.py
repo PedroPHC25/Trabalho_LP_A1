71f7c86f35ab9736c566a452bf0075917f2fb0ef
PAULA EDUARDA DE LIMA <paulaeduardalima09@gmail.com>
2023-10-13 14:31:20 -0300

    Adicione docstring nas func medidas de dispersão

--- graph_bar_management_beds.py
+++ graph_bar_management_beds.py
@@ -91,6 +91,23 @@ def graph_bar(df: pd.DataFrame, x_column: str, y_column: str,
 
 # Função recebe o df e retorna a média do número de leitos correspondente ao tipo de gestão recebido
 def mean_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> float:
+    """
+    Média do número de leitos, que pode ser LEITOS_SUS ou LEITOS_EXISTENTES, presentes em hospitais de diferentes tipos de gestão.
+
+    :param df: DataFrame.
+    :type df: pd.DataFrame
+
+    :param tipo_gestao: especiicação do tipo de gestão a ser calculada a média (Municipal, Estadual, Dupla, Sem Gestã).
+    :type tipo_gestao: str
+    
+    :return: Retorna a média em float.
+    :rtype: float
+
+    .. warning::
+    O parâmetro tipo_gestao e tipo_leito deve exister e estar ecrito corretamente
+    para encontrar a coluna dentro do df inserido.
+    
+    """
     try:
         df = df[df["TP_GESTAO"] == tipo_gestao]
         mean = df[tipo_leito].mean()
@@ -104,6 +121,23 @@ def mean_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> fl
 
 # Função recebe o df e retorna a mediana do número de leitos correspondente ao tipo de gestão recebido
 def median_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> float:
+    """
+    Mediana do número de leitos, que podem ser LEITOS_SUS ou LEITOS_EXISTENTES, presentes em hospitais de diferentes tipos de gestão.
+
+    :param df: DataFrame.
+    :type df: pd.DataFrame
+
+    :param tipo_gestao: especiicação do tipo de gestão a ser calculada a mediana (Municipal, Estadual, Dupla, Sem Gestã).
+    :type tipo_gestao: str
+    
+    :return: Retorna a mediana em float.
+    :rtype: float
+
+    .. warning::
+    O parâmetro tipo_gestao e tipo_leito deve exister e estar ecrito corretamente
+    para encontrar a coluna dentro do df inserido.
+    
+    """
     try:
         df = df[df["TP_GESTAO"] == tipo_gestao]
         median = df[tipo_leito].median()
@@ -118,6 +152,23 @@ def median_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> 
 
 # Função recebe o df e retorna o desvio padrão do número de leitos correspondente ao tipo de gestão recebido
 def std_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> float:
+    """
+    Desvio padrão do número de leitos, que podem ser LEITOS_SUS ou LEITOS_EXISTENTES, presentes em hospitais de diferentes tipos de gestão.
+
+    :param df: DataFrame.
+    :type df: pd.DataFrame
+
+    :param tipo_gestao: especiicação do tipo de gestão a ser calculada o desvio padrão (Municipal, Estadual, Dupla, Sem Gestã).
+    :type tipo_gestao: str
+    
+    :return: Retorna o desvio padrão em float.
+    :rtype: float
+
+    .. warning::
+    O parâmetro tipo_gestao e tipo_leito deve exister e estar ecrito corretamente
+    para encontrar a coluna dentro do df inserido.
+    
+    """
     try:
         df = df[df["TP_GESTAO"] == tipo_gestao]
         desvio_padrao = df[tipo_leito].std()
@@ -130,7 +181,25 @@ def std_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> flo
         print("Erro desconhecido")
 
 #Função recebe o df e retorna a quantidade de elementos distintos do número de leitos correspondente ao tipo de gestão recebido
-def unique_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> float:
+def unique_by_management(df: pd.DataFrame, tipo_gestao:str, tipo_leito: str) -> int:
+    """
+    Quantidade de elementos distintos do número de leitos, que podem ser LEITOS_SUS ou LEITOS_EXISTENTES, presentes em hospitais de
+    diferentes tipos de gestão.
+
+    :param df: DataFrame.
+    :type df: pd.DataFrame
+
+    :param tipo_gestao: especiicação do tipo de gestão a ser calculada os valores únicos (Municipal, Estadual, Dupla, Sem Gestã).
+    :type tipo_gestao: str
+    
+    :return: Retorna os valores único.
+    :rtype: int
+
+    .. warning::
+    O parâmetro tipo_gestao e tipo_leito deve exister e estar ecrito corretamente
+    para encontrar a coluna dentro do df inserido.
+    
+    """
     try:
         df = df[df["TP_GESTAO"] == tipo_gestao]
         nu_distintos = df[tipo_leito].nunique()
