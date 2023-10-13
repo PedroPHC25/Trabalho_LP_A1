acce8327db7e4482e6f65aeaf29fada80e82ee58
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-10-13 20:49:59 -0300

    Gerando o arquivo imagem do gráfico

--- drafts/data_graph_SUS.py
+++ drafts/data_graph_SUS.py
@@ -5,8 +5,15 @@ para a geração do gráfico de barras agrupadas, essas informações
 a partir desse módulo ficam explícitas em colunas próprias.
 """
 
-from df_generator import data
 import pandas as pd
+import sys
+sys.path.append('..\Trabalho_LP_A1')
 
+from df_generator import data
+from data_analysis_functions.analysis_functions_SUS import formatar_df
+from graphs_functions.graph_bar_SUS import graph_SUS
 
-print(formatar_df(data,202201, "UF", "DESC_NATUREZA_JURIDICA", "LEITOS_SUS" ))
+df_formatado = formatar_df(data,202201, "UF", "DESC_NATUREZA_JURIDICA", "LEITOS_SUS" )
+
+# print(df_formatado)
+graph_SUS(df_formatado, "Hospital Público", "Hospital Filantrópico", "Hospital Privado")
