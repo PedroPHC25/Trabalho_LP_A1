c089a1989188416c1482907894a3fb6f9967811d
eduardammag <eduardammag@gmail.com>
2023-10-14 18:46:34 -0300

    ajeitei a main

--- main.py
+++ main.py
@@ -131,6 +131,3 @@ analysis_pediatric_regions_text = apr.calcular_estatisticas(data_pediatric_regio
 
 #mostrando os dados graficamente no formato de barras
 graph_bar_pediatric_region_image = gbpr.plot_bar_chart_from_tuples(data_pediatric_regions[0],data_pediatric_regions[1])
-
-print(data_pediatric_regions)
-print(analysis_pediatric_regions_text)
