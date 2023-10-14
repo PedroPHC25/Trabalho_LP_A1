49f442a6e8dcc6b31b2ebd72a9878eeab83d77eb
eduardammag <eduardammag@gmail.com>
2023-10-14 14:26:47 -0300

    novo commint

--- analise_eduarda.py
+++ analise_eduarda.py
@@ -7,101 +7,33 @@ from df_generator import data
 data_ped = data.groupby("REGIAO")["UTI_PEDIATRICO_EXIST"].sum()
 print(data_ped)
 
-def calculate_statistics(df, y_column):
+import numpy as np
+
+def calculate_statistics(df):
     """
-    Calcula a média, mediana, desvio padrão, valor máximo e valor mínimo de uma coluna em um DataFrame.
+    Calcula a média, mediana, desvio padrão, valor máximo, valor mínimo, moda e variância de uma coluna em um DataFrame.
 
     :param df: DataFrame contendo os dados.
-    :type df: pandas.DataFrame
-    :param y_column: Nome da coluna para análise.
-    :type y_column: str
+    :type df: pandas.Series
 
     :return: Dicionário contendo as estatísticas calculadas.
     :rtype: dict
     """
-    try:
-        # Verifica se a coluna fornecida existe no DataFrame
-        assert y_column in df.columns, f"A coluna {y_column} não existe no DataFrame."
-    except AssertionError as e:
-        print(e)
-        return
-    
     # Calcula as estatísticas
     statistics = {
-        'Média': np.mean(df[y_column]),
-        'Mediana': np.median(df[y_column]),
-        'Desvio Padrão': np.std(df[y_column]),
-        'Máximo': np.max(df[y_column]),
-        'Mínimo': np.min(df[y_column])
+        'Média': np.mean(df),
+        'Mediana': np.median(df),
+        'Desvio Padrão': np.std(df),
+        'Máximo': np.max(df),
+        'Mínimo': np.min(df),
+        'Moda': df.mode()[0],  # A moda pode ter múltiplos valores, pegamos o primeiro
+        'Variância': np.var(df)
     }
     
     return statistics
 
-
-def plot_statistics(statistics):
-    """
-    Plota as estatísticas calculadas.
-
-    :param statistics: Dicionário contendo as estatísticas.
-    :type statistics: dict
-
-    :return: None
-    :rtype: None
-    """
-    labels = statistics.keys()
-    values = statistics.values()
-
-    plt.figure(figsize=(10, 6))
-    plt.bar(labels, values, color='skyblue')
-    plt.title('Estatísticas dos Leitos Pediátricos por Região')
-    plt.xlabel('Estatística')
-    plt.ylabel('Valor')
-    plt.show()
-
-
-
-
-
-
-#ANÁLISE
-
+# Agora, para calcular as estatísticas usando a função
 data_ped = data.groupby("REGIAO")["UTI_PEDIATRICO_EXIST"].sum()
-print(data_ped)
 
-# Média
-media = data_ped.mean()
-
-# Moda
-moda = data_ped.mode()[0]  # A moda pode ter múltiplos valores, pegamos o primeiro
-
-# Mediana
-mediana = data_ped.median()
-
-# Desvio Padrão
-desvio_padrao = data_ped.std()
-
-# Variância
-variancia = data_ped.var()
-
-# Valor Mínimo e Máximo
-minimo = data_ped.min()
-maximo = data_ped.max()
-
-print(f'Média: {media}')
-print(f'Moda: {moda}')
-print(f'Mediana: {mediana}')
-print(f'Desvio Padrão: {desvio_padrao}')
-print(f'Variância: {variancia}')
-print(f'Valor Mínimo: {minimo}')
-print(f'Valor Máximo: {maximo}')
-
-import matplotlib.pyplot as plt
-
-# Criar um gráfico de barras
-plt.figure(figsize=(10, 6))
-plt.bar(data_ped.index, data_ped.values, color='pink')
-plt.title('UTI Pediátrica Existente por Região')
-plt.xlabel('Região')
-plt.ylabel('UTI Pediátrica Existente')
-plt.xticks(rotation=45)  # Rotacionar os rótulos do eixo x para melhor legibilidade
-plt.show()
+statistics = calculate_statistics(data_ped)
+print(statistics)
