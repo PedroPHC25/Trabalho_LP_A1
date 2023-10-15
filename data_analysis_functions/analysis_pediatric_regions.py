a8ed77c4c40d83ec5b24dc6e03591832bbe992d4
eduardammag <eduardammag@gmail.com>
2023-10-15 11:32:03 -0300

    mudanças finais

--- data_analysis_functions/analysis_pediatric_regions.py
+++ data_analysis_functions/analysis_pediatric_regions.py
@@ -29,22 +29,3 @@ def calcular_estatisticas(tupla):
     }
     
     return estatisticas
-
-def test_calcular_estatisticas():
-    dados = (['CENTRO-OESTE', 'NORDESTE', 'NORTE', 'SUDESTE', 'SUL'], [30450, 55476, 22550, 150046, 35870])
-    estatisticas = calcular_estatisticas(dados)
-    
-    assert 'Média' in estatisticas
-    assert 'Mediana' in estatisticas
-    assert 'Desvio Padrão' in estatisticas
-    assert 'Variância' in estatisticas
-    
-    assert isinstance(estatisticas['Média'], float)
-    assert isinstance(estatisticas['Mediana'], (int, float))  # Aceita tanto inteiros quanto floats
-    assert isinstance(estatisticas['Desvio Padrão'], float)
-    assert isinstance(estatisticas['Variância'], float)
-
-
-if __name__ == "__main__":
-    test_calcular_estatisticas()
-    print("Todos os testes passaram.")
